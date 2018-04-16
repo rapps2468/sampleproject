44ee8b2720ae55d8c3f1fcbeb362865cc349a07a
Your Name <you@example.com>
2018-04-15 20:17:14 -0700

    add logging

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,18 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Formatter;
+import java.util.Properties;
+import java.util.logging.*;
 
 public class HelloWorld {
+
+    static {
+        setupLogger();
+    }
+
+    private static Logger LOGGER;
 
     public static void main(String[] args) {
         print(System.out);
@@ -8,6 +20,34 @@ public class HelloWorld {
 
     public static void print(PrintStream out) {
         out.println("Hello, World!");
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.warning("Hello, World log!");
     }
 
+    public static void setupLogger() {
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            System.setProperty("java.util.logging.SimpleFormatter.format",
+                    "%1$tF %1$tT %4$s %2$s %5$s%6$s%n");
+
+            FileHandler handler = new FileHandler("test.log", 490000, 5);
+            SimpleFormatter formatter = new SimpleFormatter();
+            handler.setFormatter(new SimpleFormatter());
+            LOGGER = Logger.getLogger(HelloWorld.class.getName());
+            LOGGER.addHandler(handler);     // for your code..
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -8,6 +8,11 @@ public class MyTest {
 
     @Test
     public void testName() throws Exception {
+
+        //TODO: (RW) Don't do this
+        //       or else
+        //
+        //END_TODO
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         HelloWorld.print(new PrintStream(out));
         java.lang.String s = out.toString();
